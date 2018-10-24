9bf7fc19bd01b69e419f4ecef6d84240fa2aa09f
Jolly <wjl_0017@163.com>
2018-10-24 14:31:42 +0800

    第一次提交

--- source/app.js
+++ source/app.js
@@ -1,21 +1,6 @@
-import React ,{Component}from 'react'
+import React from 'react'
 import React_dom from 'react-dom'
-import GroceryList from './Main/Grocerylist'
 import KanbanBoardContainer from './kankanBoard/KanbanBoardContainer'
-// import AnimatedShoppingList from './AnimatedShoppingList/AnimatedShoppingList'
-// import Container from './DragDropContext/Container'
-class Hello extends Component{
-    render(){
-        return(
-            
-            <div>
-                Hello   React
-                <GroceryList/>
-            </div>
-        );
-    }
-}
-
 
 React_dom.render(
     <KanbanBoardContainer/> ,
