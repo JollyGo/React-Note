ecb4bbd71946deb2df74eeea4c128a4d6bd888bc
Jolly <wjl_0017@163.com>
2018-10-26 18:14:56 +0800

    update Card From.js

--- source/app.js
+++ source/app.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import React_dom from 'react-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import history from 'history'
 import KanbanBoardContainer from './kankanBoard/KanbanBoardContainer'
-
+const App =()=>{
+    return(
+        <Router history={history}>
+            <Route component={KanbanBoardContainer}></Route>
+        </Router>
+    )
+}
 React_dom.render(
     <KanbanBoardContainer/> ,
     document.getElementById('root')
